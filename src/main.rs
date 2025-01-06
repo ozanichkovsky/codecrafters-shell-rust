9568663b621b2b555402cd94503d4db6cd9707e4
Oleksandr Zanichkovskyi <olexandr.zanichkovsky@zophiatech.com>
2025-01-06 13:56:26 +0100

    REPL

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,16 @@
 use std::io::{self, Write};
 
 fn main() {
-    // Uncomment this block to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        // Uncomment this block to pass the first stage
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
-    let input = input.trim();
-    println!("{}: command not found", input);
+        // Wait for user input
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+        let input = input.trim();
+        println!("{}: command not found", input);
+    }
 }
